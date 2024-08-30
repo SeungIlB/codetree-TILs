25c032de019b73946d7cc2a36f168e6709cf5770
Seung1 <122969983+SeungIlB@users.noreply.github.com>
2024-08-30 13:03:14 +0900

    Title: 달에서 무게 구하기, Time: 101ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240830/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int chu = 13;
+        double rate = 0.165000;
+        
+        
+
+        System.out.printf("13 * 0.165000 = %.6f", chu * rate);
+
+    }
+}
