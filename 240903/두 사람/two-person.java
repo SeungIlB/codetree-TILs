6a17dc8577074b13112ae0c826f081cbe68178c9
Seung1 <122969983+SeungIlB@users.noreply.github.com>
2024-09-03 22:06:52 +0900

    Title: 두 사람, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240903/두 사람/two-person.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int age1 = sc.nextInt();
+        String sex1 = sc.next();
+        int age2 = sc.nextInt();
+        String sex2 = sc.next();
+
+        if(age1 >= 19 || age2 >= 19){
+            System.out.println(1);
+        }
+    }
+}
