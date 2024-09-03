0b0b89b0541e62d5a18d3f45de0c97b8038b5421
Seung1 <122969983+SeungIlB@users.noreply.github.com>
2024-09-03 22:32:39 +0900

    Title: 윤년인가, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240903/윤년인가/is-leap-year.java
+++ 240903/윤년인가/is-leap-year.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
+        // Scanner를 사용하여 입력을 받습니다.
         Scanner sc = new Scanner(System.in);
+        
+        // 자연수 y 입력 받기
         int y = sc.nextInt();
-        if(y % 4 != 0){
-            if(y % 100 == 0 && y % 400 != 0){
-                System.out.println("false");
-            }
-        }
-        else {
-            System.out.println("true");
+        
+        // 윤년 조건을 체크합니다.
+        if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0)) {
+            System.out.println("true");  // 윤년일 경우 true 출력
+        } else {
+            System.out.println("false"); // 평년일 경우 false 출력
         }
     }
 }
